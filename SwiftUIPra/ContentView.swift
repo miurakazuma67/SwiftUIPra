b3f5af7f215df4d04fb582c69760a17e46f7f9d5
miurakazuma67 <miurakazuma67@gmail.com>
2023-12-16 10:52:10 +0900

    Initial Commit

---
+++ SwiftUIPra/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPra
+//
+//  Created by 三浦　一真 on 2023/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPra/SwiftUIPraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPraApp.swift
+//  SwiftUIPra
+//
+//  Created by 三浦　一真 on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
